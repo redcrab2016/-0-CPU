63931163d2ed7f1f355b228a1c4ffa83be0b0b33
Redcrab <promethe@promet-dsb-1.cc.cec.eu.int>
2023-01-16 19:31:20 +0100

    runtime - Assembler/debugger (still missing argc/v processing)

--- BackSlashThree/bs3_main.c
+++ BackSlashThree/bs3_main.c
@@ -1,7 +1,21 @@
+#ifdef BS3RUNTIME
+
+#include "bs3.h"
+int main(int argc, char *argv[]) 
+{
+  struct bs3_asm_code_map codemap;
+  bs3_asm_code_map_reset(&codemap);
+  bs3_asm_code_map_load("test.out.2",&codemap,0);
+  bs3_hyper_main(&codemap,((void *)0));
+  return 0;
+}
+#else
+
 #include "bs3_asm.h"
 #include "bs3_debug.h"
 
-void main() {
+int main(int argc, char *argv[]) 
+{
   struct bs3_asm_code_map codemap;
   
   if (bs3_asm_file("test.asm", "test.out.2","test.report.txt", BS3_ASM_CODE_MAP_TYPE_HEXA ) == BS3_ASM_PASS1_PARSE_ERR_OK)
@@ -12,6 +26,7 @@ void main() {
     bs3_hyper_main(&codemap,&bs3_debug);
     bs3_debug_end();
   }
-  return;
+  return 0;
  
 }
+#endif
