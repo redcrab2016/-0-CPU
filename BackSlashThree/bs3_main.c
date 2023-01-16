3c379d50be8f7bf027928d49c4bc08518c37747f
Redcrab <promethe@promet-dsb-1.cc.cec.eu.int>
2023-01-16 19:52:27 +0100

    fix

--- BackSlashThree/bs3_main.c
+++ BackSlashThree/bs3_main.c
@@ -1,11 +1,24 @@
 #ifdef BS3RUNTIME
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "bs3.h"
 int main(int argc, char *argv[]) 
 {
+  int i, err;
   struct bs3_asm_code_map codemap;
   bs3_asm_code_map_reset(&codemap);
-  bs3_asm_code_map_load("test.out.2",&codemap,0);
+  for (i = 0 ; i < argc; i++)
+  {
+    if ((err = bs3_asm_code_map_load(argv[i],&codemap,0)) != BS3_ASM_CODE_MAP_ERR_OK)
+    {
+      if (err == BS3_ASM_CODE_MAP_ERR_LOADBADFILE && (i == (argc -1)))
+      {
+        fprintf(stderr,"Error on loading %s : %s\n",argv[i],bs3_asm_code_map_message[err]);
+        return 1;
+      }
+    }
+  }
   bs3_hyper_main(&codemap,((void *)0));
   return 0;
 }
