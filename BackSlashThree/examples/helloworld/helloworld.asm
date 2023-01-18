2ae6b0ab950d3947ab609d1c00c5673d09baab22
Redcrab <promethe@promet-dsb-1.cc.cec.eu.int>
2023-01-18 19:50:49 +0100

    fix

--- BackSlashThree/examples/helloworld/helloworld.asm
+++ BackSlashThree/examples/helloworld/helloworld.asm
@@ -1,9 +1,9 @@
 ; Hello World example
-    org     $0000   
+    org     $0000       ; int_0 , cpu reset PC address location
     dw      start       ; set boot address
 
-start   org $0400
-    lean_w0 msg
+start   org $0400       ; first address usable for program
+    lean_w0 msg         ; take W0 with msg address by short offset 
 loop:                   ; for each character to print
     ld      b3, [w0]    ; get a character
     cmp     b3, 0       ; test if it is the last character
@@ -12,7 +12,7 @@ retry:
     out     b3          ; print out the character
     jz      retry       ; if output is not ready then retry
     inc     w0          ; after print out, select next character
-    j       loop        ; print out next chacter
+    j       loop        ; print out next character
 bye:    
     hlt                 ; end of Hello World, stop
 
