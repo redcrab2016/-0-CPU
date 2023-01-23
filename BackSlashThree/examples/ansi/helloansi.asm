adf9320b5f8b19fc4e3d6dcd0f65bc3767b88e82
Redcrab <promethe@promet-dsb-1.cc.cec.eu.int>
2023-01-23 19:20:31 +0100

    fix

--- BackSlashThree/examples/ansi/helloansi.asm
+++ BackSlashThree/examples/ansi/helloansi.asm
@@ -1,5 +1,5 @@
 ; Hello Ansi example
-; same as helloworld.asm but use bs3core.inc and bs3outansi.inc
+; An 'hello world' program to test ANSI feature in bs3outansi.inc
         include "bs3core.inc"           ; use bs3 core
         include "bs3outansi.inc"        ; use bs3 print out ANSI 
         mbs3_bootat     start           ; 'start' is where CPU boot
@@ -10,9 +10,24 @@ start   org             ebs3_start      ; boot at normal code addr
         ansi_SGR_fg_yellow              ; foreground color Yellow
         ansi_SGR_bg_blue                ; backgound color Blue
         mbs3_printstr "Hello "          ; print'Hello ' yellow on blue
-        ansi_SGR_fg_red                 ; foreground color Yellow
+        ansi_SGR_fg_red                 ; foreground color Red
         ansi_SGR_sblink                 ; blink
-        mbs3_printstr "ANSI"            ; print blink 'ANSI'
+        mbs3_printstr "ANSI"            ; print blink red 'ANSI'
+        ansi_SGR_blinkoff               ; stop blinking
+        ansi_CUP        5, 1            ; cursor row 8 column 1
+        ansi_SGR_fg_white               ; foreground color White
+        ansi_SGR_bg_green               ; backgound color green        
+        mbs3_println    msg
+        mbs3_println    msg
+        mbs3_println    msg
+        ansi_SGR_fg_magenta               ; foreground color magenta
+        ansi_SGR_bg_magenta               ; backgound color magenta 
+        mbs3_println    msg
         ansi_SGR_reset                  ; reset to normal
-        ansi_CUP        8, 1            ; cursor row 8 column 1
+        ansi_CUP        12, 1           ; cursor row 10 column 1
         hlt                             ; end of Hello World, stop
+
+msg     db              $E2, $96, $80 
+        db              $E2, $96, $80
+        db              $E2, $96, $80
+        db              ' ',$E2, $96, $80, 0
